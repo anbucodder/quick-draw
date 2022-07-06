3c001407cae70ec9a719dea9bf0fe999e21d004d
anbucodder <100592337+anbucodder@users.noreply.github.com>
2022-07-06 06:52:10 +0530

    Update topic.js

--- topic.js
+++ topic.js
@@ -1,40 +1,9 @@
-var timer=0;
-function setup(){
-    classifier = ml5.imageClassfier("DoodleNet");
-    console.log("ml5 vertion: "+ml5.vertion)
+const objects=["fan","basketball","football","ball","clock","ears","phone" ,"cake"," cookie","donut"]
+function topic(){
+    var topic=objects[Math.round(Math.random()*10)];
+    document.getElementById("rtopic").innerHTML="Your topic is : "+topic;  
+    localStorage.setItem("topic",topic);
 }
-function setup(){
-    canvas = createCanvas(900,400);
-    canvas.center();
-    canvas.background("white");
-    canvas.mouseReleased(classifyCanvas);
-    synth = window.speechSynthesis;
-}
-function draw(){
-    strokeWeight(5);
-    stroke(0);
-    if(mouseIsPressed){
-        line(pmouseX,pmouseY,mouseX,mouseY);
-    }
-}
-function settopic(){
-    document.getElementById("topic").innerHTML="Topic : "+localStorage.getItem('topic')
-}
-function clearCanvas(){
-    canvas.background("white");
-}
-function classifyCanvas(){
-    classifier.classify(canvas,gotResult);
-}
-function gotResult(error,results){
-    if(error){
-        console.error(error);
-    }
-    else{
-        console.log(results);
-        document.getElementById("label").innerHTML="Label : "+results[0].label;
-        document.getElementById("confidence").innerHTML="Confidence : "+Math.round(results[0].confidence*100)+"%";
-        var utterthis = new SpeechSynthesisUtterance(results[0].label);
-        synth.speek(utterthis);
-    }
+function go(){
+    window.location="main.html";
 }
